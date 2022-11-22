04fc6e59a3e4f358427706910a00f8dd907d1ebc
spidΞy <web3slinger@gmail.com>
2022-11-22 10:57:03 -0600

    Update ethpass api

--- pages/api/ethpass/create.js
+++ pages/api/ethpass/create.js
@@ -1,16 +1,61 @@
 export default async function handler(req, res) {
   switch (req.method) {
-    case 'POST':
-      const {
-        chainId,
-        contractAddress,
-        platform,
-        signature,
-        signatureMessage,
-        tokenId,
-      } = req.body
-
+    case 'GET':
       try {
+        const { contractAddress, tokenId, chainId, ownerAddress } = req.query
+
+        const response = await fetch(
+          `https://api.ethpass.xyz/api/v0/passes?contractAddress=${contractAddress}&tokenId=${tokenId}&network=${chainId}&chain=${'evm'}&ownerAddress=${ownerAddress}&expired=${0}`,
+          {
+            method: 'GET',
+            headers: new Headers({
+              'content-type': 'application/json',
+              'x-api-key': process.env.ETHPASS_API_KEY,
+            }),
+          }
+        )
+
+        if (response.status === 200) {
+          const pass = await response.json()
+
+          if (pass.length) {
+            try {
+              const response = await fetch(
+                `https://api.ethpass.xyz/api/v0/passes/${pass[0].id}/distribute`,
+                {
+                  method: 'GET',
+                  headers: new Headers({
+                    'content-type': 'application/json',
+                    'x-api-key': process.env.ETHPASS_API_KEY,
+                  }),
+                }
+              )
+
+              if (response.status === 200) {
+                const json = await response.json()
+                return res.status(200).json({ ...json, platform: pass[0].platform })
+              } else {
+                throw Error
+              }
+            } catch (error) {
+              return res.status(400).send(error.message)
+            }
+          } else {
+            return res.status(200).json(pass)
+          }
+        } else {
+          const json = await response.json()
+          return res.status(response.status).send(json.message)
+        }
+      } catch (error) {
+        return res.status(400).send(error.message)
+      }
+
+    case 'POST':
+      try {
+        const { contractAddress, tokenId, chainId, platform, signature, signatureMessage } =
+          req.body
+
         // Customize Pass
         let pass
         if (platform === 'apple') {
@@ -34,10 +79,7 @@ export default async function handler(req, res) {
               {
                 key: 'secondary1',
                 label: 'CONTRACT ADDRESS',
-                value: `${contractAddress.slice(
-                  0,
-                  6
-                )}...${contractAddress.slice(-4)}`,
+                value: `${contractAddress.slice(0, 6)}...${contractAddress.slice(-4)}`,
                 textAlignment: 'PKTextAlignmentLeft',
               },
               {
@@ -61,34 +103,32 @@ export default async function handler(req, res) {
         }
 
         // Request to create pass
-        const payload = await fetch(
-          `${process.env.API_HOST || 'https://api.ethpass.xyz'}/api/v0/passes`,
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              chain: {
-                name: 'evm',
-                network: chainId,
-              },
-              nft: {
-                contractAddress,
-                tokenId,
-              },
-              pass,
-              platform,
-              signature,
-              signatureMessage,
-              barcode: {
-                message: 'Payload returned after successfully scanning a pass',
-              },
-              image: '',
-            }),
-            headers: new Headers({
-              'content-type': 'application/json',
-              'x-api-key': process.env.ETHPASS_API_KEY,
-            }),
-          }
-        )
+        const payload = await fetch('https://api.ethpass.xyz/api/v0/passes', {
+          method: 'POST',
+          body: JSON.stringify({
+            chain: {
+              name: 'evm',
+              network: chainId,
+            },
+            nft: {
+              contractAddress,
+              tokenId,
+            },
+            pass,
+            platform,
+            signature,
+            signatureMessage,
+            barcode: {
+              message: 'Payload returned after successfully scanning a pass',
+            },
+            image: '',
+          }),
+          headers: new Headers({
+            'content-type': 'application/json',
+            'x-api-key': process.env.ETHPASS_API_KEY,
+          }),
+        })
+
         if (payload.status === 200) {
           const json = await payload.json()
           return res.status(200).json(json)
@@ -101,7 +141,7 @@ export default async function handler(req, res) {
       }
 
     default:
-      res.setHeader('Allow', ['POST'])
+      res.setHeader('Allow', ['GET', 'POST'])
       res.status(405).end(`Method ${req.method} Not Allowed`)
       break
   }
